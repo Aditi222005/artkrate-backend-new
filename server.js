a627fdca4cfe23d29de449d32502b64511baa72d
joshiAditi2005 <aditisjoshi2005@gmail.com>
2026-06-10 12:02:54 +0530

    fix: cors error fixed

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ const allowedOrigins = [
   process.env.CLIENT_URL || 'http://localhost:5173',
   'http://localhost:8080',
   'http://localhost:5174',
+  'http://localhost:8081',
+  'http://localhost:8082',
 ];
 
 app.use(
